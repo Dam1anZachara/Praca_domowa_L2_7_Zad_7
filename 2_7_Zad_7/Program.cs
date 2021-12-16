43b1ba7ff9bc7367c9f9a9f6170d6c59e72d0b1d
dzachara <dzachara@ZACHARA-D>
2021-12-16 13:38:40 +0100

    Add project files.

---
+++ 2_7_Zad_7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2_7_Zad_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program pobierze 3 liczby i sprawdzi, która jest największa.");
+            Console.Write("Podaj pierwszą liczbę: ");
+            Int32.TryParse(Console.ReadLine(), out int firstNumber);
+
+            Console.Write("Podaj drugą liczbę: ");
+            Int32.TryParse(Console.ReadLine(), out int secondNumber);
+
+            Console.Write("Podaj trzecią liczbę: ");
+            Int32.TryParse(Console.ReadLine(), out int thirdNumber);
+
+            if ((firstNumber > secondNumber && firstNumber > thirdNumber) || (firstNumber == secondNumber && firstNumber > thirdNumber))
+            {
+                Console.WriteLine($"{firstNumber} jest największa z podanych.");
+            }
+            else if ((secondNumber > firstNumber && secondNumber > thirdNumber) || (secondNumber == thirdNumber && secondNumber > firstNumber))
+            {
+                Console.WriteLine($"{secondNumber} jest największa z podanych.");
+            }
+            else if ((thirdNumber > firstNumber && thirdNumber > secondNumber) || (thirdNumber == firstNumber && thirdNumber > secondNumber))
+            {
+                Console.WriteLine($"{thirdNumber} jest największa z podanych.");
+            }
+        }
+    }
+}
